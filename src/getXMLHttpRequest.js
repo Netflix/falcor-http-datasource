71b0d8eb6a4939ec6c7129d0b58284d142c1f065
gdi2290 <github@gdi2290.com>
2015-05-25 23:19:33 -0700

    feat: getXMLHttpRequest

---
+++ src/getXMLHttpRequest.js
@@ -0,0 +1,24 @@
+'use strict';
+module.exports = function getXMLHttpRequest() {
+    var progId,
+        progIds,
+        i;
+    if (global.XMLHttpRequest) {
+      return new global.XMLHttpRequest();
+    } else {
+      try {
+        progIds = ['Msxml2.XMLHTTP', 'Microsoft.XMLHTTP', 'Msxml2.XMLHTTP.4.0'];
+        for (i = 0; i < 3; i++) {
+            try {
+                progId = progIds[i];
+                if (new global.ActiveXObject(progId)) {
+                    break;
+                }
+            } catch(e) { }
+        }
+        return new global.ActiveXObject(progId);
+      } catch (e) {
+        throw new Error('XMLHttpRequest is not supported by your browser');
+      }
+    }
+};

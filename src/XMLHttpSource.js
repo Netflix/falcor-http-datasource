b67a0d1fef7c2316fa49f33e51ae3c8845cf517f
gdi2290 <github@gdi2290.com>
2015-06-18 15:43:48 -0700

    chore: convert to two spaces

--- src/XMLHttpSource.js
+++ src/XMLHttpSource.js
@@ -6,89 +6,89 @@ var buildQueryObject = require('./buildQueryObject');
 var isArray = Array.isArray;
 
 function simpleExtend(obj, obj2) {
-    var prop;
-    for (prop in obj2) {
-        obj[prop] = obj2[prop];
-    }
-    return obj;
+  var prop;
+  for (prop in obj2) {
+    obj[prop] = obj2[prop];
+  }
+  return obj;
 }
 
 function XMLHttpSource(jsongUrl, config) {
-    this._jsongUrl = jsongUrl;
-    if (typeof config === 'number') {
-        var newConfig = {
-            timeout: config
-        };
-        config = newConfig;
-    }
-    this._config = simpleExtend({
-        timeout: 15000,
-        headers: {}
-    }, config || {});
+  this._jsongUrl = jsongUrl;
+  if (typeof config === 'number') {
+    var newConfig = {
+      timeout: config
+    };
+    config = newConfig;
+  }
+  this._config = simpleExtend({
+    timeout: 15000,
+    headers: {}
+  }, config || {});
 }
 
 XMLHttpSource.prototype = {
-    // because javascript
-    constructor: XMLHttpSource,
-    /**
-     * buildQueryObject helper
-     */
-    buildQueryObject: buildQueryObject,
+  // because javascript
+  constructor: XMLHttpSource,
+  /**
+   * buildQueryObject helper
+   */
+  buildQueryObject: buildQueryObject,
 
-    /**
-     * @inheritDoc DataSource#get
-     */
-    get: function httpSourceGet(pathSet) {
-        var method = 'GET';
-        var queryObject = this.buildQueryObject(this._jsongUrl, method, {
-            path: pathSet,
-            method: 'get'
-        });
-        var config = simpleExtend(queryObject, this._config);
-        // pass context for onBeforeRequest callback
-        var context = this;
-        return request(method, config, context);
-    },
+  /**
+   * @inheritDoc DataSource#get
+   */
+  get: function httpSourceGet(pathSet) {
+    var method = 'GET';
+    var queryObject = this.buildQueryObject(this._jsongUrl, method, {
+      path: pathSet,
+      method: 'get'
+    });
+    var config = simpleExtend(queryObject, this._config);
+    // pass context for onBeforeRequest callback
+    var context = this;
+    return request(method, config, context);
+  },
 
-    /**
-     * @inheritDoc DataSource#set
-     */
-    set: function httpSourceSet(jsongEnv) {
-        var method = 'POST';
-        var queryObject = this.buildQueryObject(this._jsongUrl, method, {
-            jsong: jsongEnv,
-            method: 'set'
-        });
-        var config = simpleExtend(queryObject, this._config);
-        // pass context for onBeforeRequest callback
-        var context = this;
-        return request(method, config, context);
+  /**
+   * @inheritDoc DataSource#set
+   */
+  set: function httpSourceSet(jsongEnv) {
+    var method = 'POST';
+    var queryObject = this.buildQueryObject(this._jsongUrl, method, {
+      jsong: jsongEnv,
+      method: 'set'
+    });
+    var config = simpleExtend(queryObject, this._config);
+    // pass context for onBeforeRequest callback
+    var context = this;
+    return request(method, config, context);
 
-    },
+  },
 
-    /**
-     * @inheritDoc DataSource#call
-     */
-    call: function httpSourceCall(callPath, args, pathSuffix, paths) {
-        // arguments defaults
-        args = args || [];
-        pathSuffix = pathSuffix || [];
-        paths = paths || [];
+  /**
+   * @inheritDoc DataSource#call
+   */
+  call: function httpSourceCall(callPath, args, pathSuffix, paths) {
+    // arguments defaults
+    args = args || [];
+    pathSuffix = pathSuffix || [];
+    paths = paths || [];
 
-        var method = 'POST';
-        var queryData = [];
-        queryData.push('method=call');
-        queryData.push('callPath=' + encodeURIComponent(JSON.stringify(callPath)));
-        queryData.push('arguments=' + encodeURIComponent(JSON.stringify(args)));
-        queryData.push('pathSuffixes=' + encodeURIComponent(JSON.stringify(pathSuffix)));
-        queryData.push('paths=' + encodeURIComponent(JSON.stringify(paths)));
+    var method = 'POST';
+    var queryData = [];
+    queryData.push('method=call');
+    queryData.push('callPath=' + encodeURIComponent(JSON.stringify(callPath)));
+    queryData.push('arguments=' + encodeURIComponent(JSON.stringify(args)));
+    queryData.push('pathSuffixes=' + encodeURIComponent(JSON.stringify(pathSuffix)));
+    queryData.push('paths=' + encodeURIComponent(JSON.stringify(paths)));
 
-        var queryObject = this.buildQueryObject(this._jsongUrl, method, queryData.join('&'));
-        var config = simpleExtend(queryObject, this._config);
-        // pass context for onBeforeRequest callback
-        var context = this;
-        return request(method, config, context);
-    }
+    var queryObject = this.buildQueryObject(this._jsongUrl, method, queryData.join('&'));
+    var config = simpleExtend(queryObject, this._config);
+    // pass context for onBeforeRequest callback
+    var context = this;
+    return request(method, config, context);
+  }
 };
 // ES6 modules
 XMLHttpSource.XMLHttpSource = XMLHttpSource;
--- src/request.js
+++ src/request.js
@@ -5,174 +5,174 @@ var getCORSRequest = require('./getCORSRequest');
 var hasOwnProp = Object.prototype.hasOwnProperty;
 
 function request(method, options, context) {
-    return Observable.create(function requestObserver(observer) {
-        var config = {
-            method: method || 'GET',
-            crossDomain: false,
-            async: true,
-            headers: {},
-            responseType: 'json'
-        };
-        var xhr,
-            isDone,
-            headers,
-            header,
-            prop;
+  return Observable.create(function requestObserver(observer) {
+    var config = {
+      method: method || 'GET',
+      crossDomain: false,
+      async: true,
+      headers: {},
+      responseType: 'json'
+    };
+    var xhr,
+      isDone,
+      headers,
+      header,
+      prop;
 
-        for (prop in options) {
-            if (hasOwnProp.call(options, prop)) {
-                config[prop] = options[prop];
-            }
+    for (prop in options) {
+      if (hasOwnProp.call(options, prop)) {
+        config[prop] = options[prop];
+      }
+    }
+
+    // Add request with Headers
+    if (!config.crossDomain && !config.headers['X-Requested-With']) {
+      config.headers['X-Requested-With'] = 'XMLHttpRequest';
+    }
+
+    // allow the user to mutate the config open
+    if (context.onBeforeRequest != null) {
+      context.onBeforeRequest(config);
+    }
+
+    // create xhr
+    try {
+      xhr = config.crossDomain ? getCORSRequest() : getXMLHttpRequest();
+    } catch (err) {
+      observer.onError(err);
+    }
+    try {
+      // Takes the url and opens the connection
+      if (config.user) {
+        xhr.open(config.method, config.url, config.async, config.user, config.password);
+      } else {
+        xhr.open(config.method, config.url, config.async);
+      }
+
+      // Sets timeout information
+      xhr.timeout = config.timeout;
+
+      // Anything but explicit false results in true.
+      xhr.withCredentials = config.withCredentials !== false;
+
+      // Fills the request headers
+      headers = config.headers;
+      for (header in headers) {
+        if (hasOwnProp.call(headers, header)) {
+          xhr.setRequestHeader(header, headers[header]);
         }
+      }
 
-        // Add request with Headers
-        if (!config.crossDomain && !config.headers['X-Requested-With']) {
-          config.headers['X-Requested-With'] = 'XMLHttpRequest';
-        }
-
-        // allow the user to mutate the config open
-        if (context.onBeforeRequest != null) {
-            context.onBeforeRequest(config);
-        }
-
-        // create xhr
+      if (config.responseType) {
         try {
-            xhr = config.crossDomain ? getCORSRequest() : getXMLHttpRequest();
-        } catch (err) {
-            observer.onError(err);
-        }
-        try {
-            // Takes the url and opens the connection
-            if (config.user) {
-                xhr.open(config.method, config.url, config.async, config.user, config.password);
-            } else {
-                xhr.open(config.method, config.url, config.async);
-            }
-
-            // Sets timeout information
-            xhr.timeout = config.timeout;
-
-            // Anything but explicit false results in true.
-            xhr.withCredentials = config.withCredentials !== false;
-
-            // Fills the request headers
-            headers = config.headers;
-            for (header in headers) {
-                if (hasOwnProp.call(headers, header)) {
-                    xhr.setRequestHeader(header, headers[header]);
-                }
-            }
-
-            if (config.responseType) {
-                try {
-                    xhr.responseType = config.responseType;
-                } catch (e) {
-                    // WebKit added support for the json responseType value on 09/03/2013
-                    // https://bugs.webkit.org/show_bug.cgi?id=73648. Versions of Safari prior to 7 are
-                    // known to throw when setting the value "json" as the response type. Other older
-                    // browsers implementing the responseType
-                    //
-                    // The json response type can be ignored if not supported, because JSON payloads are
-                    // parsed on the client-side regardless.
-                    if (responseType !== 'json') {
-                        throw e;
-                    }
-                }
-            }
-
-            xhr.onreadystatechange = function onreadystatechange(e) {
-                // Complete
-                if (xhr.readyState === 4) {
-                    if (!isDone) {
-                        isDone = true;
-                        onXhrLoad(observer, xhr, status, e);
-                    }
-                }
-            };
-
-            // Timeout
-            xhr.ontimeout = function ontimeout(e) {
-                if (!isDone) {
-                    isDone = true;
-                    onXhrError(observer, xhr, 'timeout error', e);
-                }
-            };
-
-            // Send Request
-            xhr.send(config.data);
-
+          xhr.responseType = config.responseType;
         } catch (e) {
-            observer.onError(e);
+          // WebKit added support for the json responseType value on 09/03/2013
+          // https://bugs.webkit.org/show_bug.cgi?id=73648. Versions of Safari prior to 7 are
+          // known to throw when setting the value "json" as the response type. Other older
+          // browsers implementing the responseType
+          //
+          // The json response type can be ignored if not supported, because JSON payloads are
+          // parsed on the client-side regardless.
+          if (responseType !== 'json') {
+            throw e;
+          }
         }
-        // Dispose
-        return function dispose() {
-            // Doesn't work in IE9
-            if (!isDone && xhr.readyState !== 4) {
-                isDone = true;
-                xhr.abort();
-            }
-        };//Dispose
-    });
+      }
+
+      xhr.onreadystatechange = function onreadystatechange(e) {
+        // Complete
+        if (xhr.readyState === 4) {
+          if (!isDone) {
+            isDone = true;
+            onXhrLoad(observer, xhr, status, e);
+          }
+        }
+      };
+
+      // Timeout
+      xhr.ontimeout = function ontimeout(e) {
+        if (!isDone) {
+          isDone = true;
+          onXhrError(observer, xhr, 'timeout error', e);
+        }
+      };
+
+      // Send Request
+      xhr.send(config.data);
+
+    } catch (e) {
+      observer.onError(e);
+    }
+    // Dispose
+    return function dispose() {
+      // Doesn't work in IE9
+      if (!isDone && xhr.readyState !== 4) {
+        isDone = true;
+        xhr.abort();
+      }
+    };//Dispose
+  });
 }
 
 /*
  * General handling of ultimate failure (after appropriate retries)
  */
 function _handleXhrError(observer, textStatus, errorThrown) {
-    // IE9: cross-domain request may be considered errors
-    if (!errorThrown) {
-        errorThrown = new Error(textStatus);
-    }
+  // IE9: cross-domain request may be considered errors
+  if (!errorThrown) {
+    errorThrown = new Error(textStatus);
+  }
 
-    observer.onError(errorThrown);
+  observer.onError(errorThrown);
 }
 
 function onXhrLoad(observer, xhr, status, e) {
-    var responseData,
-        responseObject;
-        // responseType;
+  var responseData,
+    responseObject;
+    // responseType;
 
-    // If there's no observer, the request has been (or is being) cancelled.
-    if (xhr && observer) {
-        // responseText is the old-school way of retrieving response (supported by IE8 & 9)
-        // response/responseType properties were introduced in XHR Level2 spec (supported by IE10)
-        responseData = ('response' in xhr) ? xhr.response : xhr.responseText;
+  // If there's no observer, the request has been (or is being) cancelled.
+  if (xhr && observer) {
+    // responseText is the old-school way of retrieving response (supported by IE8 & 9)
+    // response/responseType properties were introduced in XHR Level2 spec (supported by IE10)
+    responseData = ('response' in xhr) ? xhr.response : xhr.responseText;
 
-        // normalize IE9 bug (http://bugs.jquery.com/ticket/1450)
-        var status = (xhr.status === 1223) ? 204 : xhr.status;
+    // normalize IE9 bug (http://bugs.jquery.com/ticket/1450)
+    var status = (xhr.status === 1223) ? 204 : xhr.status;
 
-        if (status >= 200 && status <= 399) {
-            try {
-                responseData = JSON.parse(responseData || '');
-            } catch (e) {
-                _handleXhrError(observer, 'invalid json', e);
-            }
-            observer.onNext(responseData);
-            observer.onCompleted();
-            return;
+    if (status >= 200 && status <= 399) {
+      try {
+        responseData = JSON.parse(responseData || '');
+      } catch (e) {
+        _handleXhrError(observer, 'invalid json', e);
+      }
+      observer.onNext(responseData);
+      observer.onCompleted();
+      return;
 
-        } else if (status === 401 || status === 403 || status === 407) {
+    } else if (status === 401 || status === 403 || status === 407) {
 
-            return _handleXhrError(observer, responseData);
+      return _handleXhrError(observer, responseData);
 
-        } else if (status === 410) {
-            // TODO: Retry ?
-            return _handleXhrError(observer, responseData);
+    } else if (status === 410) {
+      // TODO: Retry ?
+      return _handleXhrError(observer, responseData);
 
-        } else if (status === 408 || status === 504) {
-            // TODO: Retry ?
-            return _handleXhrError(observer, responseData);
+    } else if (status === 408 || status === 504) {
+      // TODO: Retry ?
+      return _handleXhrError(observer, responseData);
 
-        } else {
+    } else {
 
-            return _handleXhrError(observer, responseData || ('Response code ' + status));
+      return _handleXhrError(observer, responseData || ('Response code ' + status));
 
-        }//if
     }//if
+  }//if
 }//onXhrLoad
 
 function onXhrError(observer, xhr, status, e) {
-    _handleXhrError(observer, status || xhr.statusText || 'request error', e);
+  _handleXhrError(observer, status || xhr.statusText || 'request error', e);
 }
 
 module.exports = request;

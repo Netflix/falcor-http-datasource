ed0b63aa23115e2ef33767e272ed057b70788407
Shahrul Nizam <hottincup@gmail.com>
2015-09-12 23:37:24 +0800

    rename module ref

---
+++ ts/falcor-http-datasource.d.ts
@@ -0,0 +1,18 @@
+declare module "falcor-http-datasource" {
+  function get(pathSet: any): any;
+  function set(jsongEnv): any;
+  function call(callPath: any, args?: any, pathSuffix?: any, paths?: any): any;
+
+  interface IXMLHttpSource {
+    get(pathSet: any): any;
+    set(jsongEnv): any;
+    call(callPath: any, args?: any, pathSuffix?: any, paths?: any): any;
+  }
+  export class XMLHttpSource implements IXMLHttpSource {
+    constructor(...args);
+    get(pathSet: any): any;
+    set(jsongEnv): any;
+    call(callPath: any, args?: any, pathSuffix?: any, paths?: any): any;
+  }
+  export default XMLHttpSource
+}
